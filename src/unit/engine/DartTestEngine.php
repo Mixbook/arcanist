ca639b615e44dae10412ef88b8033055d89a55e5
Dan Schultz <schultz.t.dan@gmail.com>
2014-02-10 03:34:16 -0800

    Only run Dart unit tests if a Dart file has changed

--- src/unit/engine/DartTestEngine.php
+++ src/unit/engine/DartTestEngine.php
@@ -8,29 +8,35 @@
 final class DartTestEngine extends ArcanistBaseUnitTestEngine {
 
   public function run() {
-    $cmd_line = 'dart tool/hop_runner.dart test';
-    $future = new ExecFuture('%C', $cmd_line);
-    list($code, $stdout, $stderr) = $future->resolve();
+    $dart_paths = array_filter($this->getPaths(), function($path) {
+      return substr($path, -5) == '.dart';
+    });
 
     $results = array();
 
-    if ($code > 0) {
-      preg_match_all("/(ERROR|FAIL):  ([\s\S]+?)(test -|test:)/", $stdout, $matches, PREG_SET_ORDER);
+    if (!empty($dart_paths)) {
+      $cmd_line = 'dart tool/hop_runner.dart test';
+      $future = new ExecFuture('%C', $cmd_line);
+      list($code, $stdout, $stderr) = $future->resolve();
 
-      foreach ($matches as $match) {
-        $result = new ArcanistUnitTestResult();
-        $result->setUserData($match[2]);
+      if ($code > 0) {
+        preg_match_all("/(ERROR|FAIL):  ([\s\S]+?)(test -|test:)/", $stdout, $matches, PREG_SET_ORDER);
 
-        switch ($match[1]) {
-          case "ERROR":
-            $result->setResult(ArcanistUnitTestResult::RESULT_BROKEN);
-            break;
-          case "FAIL":
-            $result->setResult(ArcanistUnitTestResult::RESULT_FAIL);
-            break;
+        foreach ($matches as $match) {
+          $result = new ArcanistUnitTestResult();
+          $result->setUserData($match[2]);
+
+          switch ($match[1]) {
+            case "ERROR":
+              $result->setResult(ArcanistUnitTestResult::RESULT_BROKEN);
+              break;
+            case "FAIL":
+              $result->setResult(ArcanistUnitTestResult::RESULT_FAIL);
+              break;
+          }
+
+          $results[] = $result;
         }
-
-        $results[] = $result;
       }
     }
 
